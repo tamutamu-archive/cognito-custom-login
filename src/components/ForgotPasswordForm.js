c85bee853ec1bb7e93cc8ab2fd74678c2b76e424
monasir1 <mohammed.nasir@osi.ca.gov>
2020-06-11 20:22:03 -0700

    Making recommended changes

--- src/components/ForgotPasswordForm.js
+++ src/components/ForgotPasswordForm.js
@@ -1,22 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import UserMessage from './UserMessage'
-import { Button } from '@cwds/components'
+import { Button } from '@cwds/reactstrap'
 
-const ForgotPasswordForm = ({errorMsg, email, onChange, onSubmit, disableResetPassword, onCancel, inputRef}) => {
+const ForgotPasswordForm = ({
+  errorMsg,
+  email,
+  onChange,
+  onSubmit,
+  disableResetPassword,
+  onCancel,
+  inputRef
+}) => {
   return (
     <form>
-      <h1>Password Reset</h1>
-      <UserMessage errorMessage={errorMsg}/>
-      <br/>
-      <label htmlFor='email' className='label-customizable'>Enter your login email below and we will send a message to reset your password</label>
-      <input name="emal" ref={inputRef} id="email" className="form-control inputField-customizable" type="text"
-        placeholder="Email" value={email} onChange={onChange} aria-labelledby='email_label' tabIndex="1"/>
-      <div className= 'submit-block'>
-        <Button
-          type='button'
-          id="cancelButton"
-          onClick={onCancel}>
+      <h1 style={{ color: 'red' }}>Admin Use Only</h1>
+      <UserMessage errorMessage={errorMsg} />
+      <br />
+      <label htmlFor='email' className='label-customizable'>
+        Enter your login email below and we will send a message to reset your
+        password
+      </label>
+      <input
+        name='emal'
+        ref={inputRef}
+        id='email'
+        className='form-control inputField-customizable'
+        type='text'
+        placeholder='Email'
+        value={email}
+        onChange={onChange}
+        aria-labelledby='email_label'
+        tabIndex='1'
+      />
+      <div className='submit-block'>
+        <Button type='button' id='cancelButton' onClick={onCancel}>
           Cancel
         </Button>
         <Button
@@ -25,7 +43,8 @@ const ForgotPasswordForm = ({errorMsg, email, onChange, onSubmit, disableResetPa
           color='primary'
           disabled={!email || disableResetPassword}
           onClick={onSubmit}
-          tabIndex="2">
+          tabIndex='2'
+        >
           {disableResetPassword ? 'Loading....' : 'Reset my password'}
         </Button>
       </div>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,26 +3,80 @@ import PropTypes from 'prop-types'
 import UserMessage from './UserMessage'
 import PasswordInput from './PasswordInput'
 
-const LoginForm = ({onSubmit, errorMsg, successMessage, email, password,
-  onEmailChange, onPasswordChange, disableSignIn, inputRef}) => {
+const LoginForm = ({
+  onSubmit,
+  errorMsg,
+  successMessage,
+  email,
+  password,
+  onEmailChange,
+  onPasswordChange,
+  disableSignIn,
+  inputRef
+}) => {
   return (
     <form>
-      <span className='textDescription-customizable'><h1>Log In</h1></span>
-      <UserMessage errorMessage={errorMsg} successMessage={successMessage}/>
-      <label className='label-customizable' htmlFor='email'>Email</label>
-      <input id='email' ref={inputRef} name='email' type='text' className='form-control inputField-customizable'
-        placeholder='Email' value={email} onChange={onEmailChange}/>
-      <br/>
-      <label className='label-customizable' htmlFor='password'>Password</label>
-      <PasswordInput id='password' placeholder='Password' password={password} onChange={onPasswordChange}/>
-      <a className='redirect-customizable' href='/forgotPassword'>Forgot your password?</a>
-      <button className="btn btn-primary submitButton-customizable" id="submit" type="submit" onClick={onSubmit} disabled={!(email && password)}>
+      <span className='textDescription-customizable'>
+        <h1>Log In</h1>
+      </span>
+      <UserMessage errorMessage={errorMsg} successMessage={successMessage} />
+      <label className='label-customizable' htmlFor='email'>
+        Email
+      </label>
+      <input
+        id='email'
+        ref={inputRef}
+        name='email'
+        type='text'
+        className='form-control inputField-customizable'
+        placeholder='Email'
+        value={email}
+        onChange={onEmailChange}
+      />
+      <br />
+      <label className='label-customizable' htmlFor='password'>
+        Password
+      </label>
+      <PasswordInput
+        id='password'
+        placeholder='Password'
+        password={password}
+        onChange={onPasswordChange}
+      />
+      {/* <a className='redirect-customizable' href='/forgotPassword'>Forgot your password?</a> */}
+      <button
+        className='btn btn-primary submitButton-customizable'
+        id='submit'
+        type='submit'
+        onClick={onSubmit}
+        disabled={!(email && password)}
+      >
         {disableSignIn ? 'Loading....' : 'Sign In'}
       </button>
-      <br/>
-      <br/>
-      <p className='Notice-This-system'>Notice:<br/>
-        This system is the property of the State of California and may be accessed only by authorized users. Unauthorized use of this system is strictly prohibited and may result in, but is not limited to, disciplinary action and criminal prosecution. The State of California may monitor any activity or communication on the system and retrieve any information stored within the system. By accessing and using this system, you are consenting to such monitoring and information retrieval for law enforcement and other purposes. Users have no expectation of privacy as to any communication on, or to any information stored within the system, or to any devices used to access this system.</p>
+      <br />
+      <br />
+      <p className='Notice-This-system'>
+        Notice:
+        <br />
+        This system is the property of the State of California and may be
+        accessed only by authorized users. Unauthorized use of this system is
+        strictly prohibited and may result in, but is not limited to,
+        disciplinary action and criminal prosecution. The State of California
+        may monitor any activity or communication on the system and retrieve any
+        information stored within the system. By accessing and using this
+        system, you are consenting to such monitoring and information retrieval
+        for law enforcement and other purposes. Users have no expectation of
+        privacy as to any communication on, or to any information stored within
+        the system, or to any devices used to access this system.
+      </p>
+      <p>
+        Do Not Use
+        <span style={{ paddingLeft: '5px' }}>
+          <a className='redirect-customizable' href='/forgotPassword'>
+            Training Admins Only
+          </a>
+        </span>
+      </p>
     </form>
   )
 }
